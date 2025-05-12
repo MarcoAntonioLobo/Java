e4d202eaaf5ee2c462d53da8c8bb30040f869b15
MarcoAntonioLobo <marcoantoniolobo82@gmail.com>
2025-05-12 23:31:57 +0100

    Update

--- JogoAdivinhacao.java
+++ JogoAdivinhacao.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class JogoAdivinhacao {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args)    {   Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         boolean jogarNovamente;
 
