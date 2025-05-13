dbc967f4ff2e101c485d08572ea6f43365935688
MarcoAntonioLobo <marcoantoniolobo82@gmail.com>
2025-05-13 23:06:29 +0100

    corregindo erros

--- JogoAdivinhacao.java
+++ JogoAdivinhacao.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;
 import java.util.Random;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class JogoAdivinhacao {
 
     public static void main(String[] args) {
